75649fbf31b4b8e6399ddbdce2c870592c3b6ab7
Rohit Singh <kailash.jc.rs@gmail.com>
2023-05-06 22:32:36 +0530

    updated

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     email: { type: String },
     password: { type: String },
+    userID: String
 },
     {
         versionKey: false
